070024c442ac23febc27adc25d30cf3f57c7bb55
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-02-07 09:48:45 +0800

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+inputText = document.getElementById("validation-input");
+
+inputText.addEventListener('blur', checkText);
+
+function checkText(){
+  inputText = this.value.trim();
+
+  if (inputText.length === 6){
+    
+  };
+};
